1077f96681a2bdd6856de47ad5780f3a5d5f6fb6
runiteking1 <marshall.jiang@gmail.com>
2014-04-30 20:24:16 -0400

    Enron driver

---
+++ topics/driver_enron.jl
@@ -0,0 +1,10 @@
+include("anchor_topic.jl")
+
+# Load the data
+(words, Q) = load_uci("enron")
+
+# Run the topic mining
+(p, r, A, TW) = mine_topics(Q)
+
+# Write the results files
+write_topics("topics_enron.txt", words, p, r, A, T
